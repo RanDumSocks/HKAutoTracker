39e270912aaf4ed7d4ffb3323cc2b3cec8b6f780
RanDumSocks <justinchance@randumsocks.com>
2022-01-17 13:21:11 -0800

    added local tracker

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ const fs = require('fs')
 const path = require('path')
 const root = path.resolve(process.env.APPDATA, "../LocalLow/Team Cherry/Hollow Knight/Randomizer 4/Recent/")
 const helperLog = path.resolve(root, "HelperLog.txt")
+const modLog = path.resolve(root, "../../ModLog.txt")
 const output = "HKAutotrack.md"
+const lastOut = "localTracker.md"
 
 const r_helperLocation = /[a-zA-Z0-9_]*(?=\[)/
 const r_locationLogic = /[a-zA-Z0-9_]*(?=(\[| |$))/
+
+var transitionTable = {}
+var checkTable = {}
+var avaliableTransitionTable = {}
 
 const special = {
    Crossroads_04: [ 'Salubra Bench', 'bench' ],
@@ -44,7 +50,7 @@ const special = {
    White_Palace_01: [ 'White Palace Entrance', 'bench' ],
    Fungus3_40: [ 'Gardens Stag Station', 'bench' ],
    Fungus3_50: [ 'Gardens Toll Bench', 'bench' ],
-   Deepnest_Spider_Town: [ 'Beast', 's Den' ],
+   Deepnest_Spider_Town: [ 'Beast\'s Den', undefined ],
    Deepnest_14: [ 'Failed Tramway Bench', 'bench' ],
    Room_Slug_Shrine: [ 'Unn Bench', 'bench' ],
    White_Palace_06: [ 'White Palace Balcony', 'bench' ],
@@ -68,10 +74,8 @@ classDef check color:#3ab020;
 classDef last fill:#022e00;
 `
 
-var locationDataRaw = fs.readFileSync('locations.json')
-var locationData = JSON.parse(locationDataRaw)
-var termsDataRaw = fs.readFileSync('terms.json')
-var termsData = JSON.parse(termsDataRaw)
+var locationData = JSON.parse(fs.readFileSync('locations.json'))
+var termsData = JSON.parse(fs.readFileSync('terms.json'))
 var locationLogic = {}
 
 termsData.push("RIGHTBALDURS")
@@ -86,8 +90,12 @@ for (const location of locationData) {
 async function start() {
    console.log("...Starting tracker...")
    updateTracker()
+   updateLocation()
    fs.watchFile(helperLog, { interval: 500 }, async (curr, prev) => {
       updateTracker()
+   })
+   fs.watchFile(modLog, { interval: 500 }, async (curr, prev) => {
+      updateLocation()
    })
    console.log("Tracker running, you may now minimise this window.")
 }
@@ -96,7 +104,6 @@ function updateTracker() {
    var transitionData = ""
    var checkData = ""
    var lastTransition = ""
-   var transitionTable = {}
    const helperLogFile = fs.readFileSync(helperLog, 'utf-8').replaceAll(/\*/g, "")
 
    var startInfo = false
@@ -118,8 +125,8 @@ function updateTracker() {
             var transitionTo = trimmedLine.match(r_transitionTo)[0]
             var doorFrom = trimmedLine.match(r_doorTransitions)[0]
             var doorTo = trimmedLine.match(r_doorTransitions)[1]
-            if ( transitionTo && transitionFrom) {
-               lastTransition = `${transitionTo}:::last\n`
+            if (transitionTo && transitionFrom) {
+               lastTransition = `${transitionFrom}:::last`
                if (!transitionTable[transitionFrom]) { transitionTable[transitionFrom] = {} }
                transitionTable[transitionFrom][doorFrom] = [transitionTo, doorTo]
             }
@@ -137,6 +144,7 @@ function updateTracker() {
          } else {
             var transitionLocation = line.match(r_helperLocation)[0]
             checkData += `${transitionLocation}:::transition\n`
+            avaliableTransitionTable[transitionLocation] = true
          }
       }
       if (!startInfo && r_transStart.test(line)) {
@@ -149,6 +157,7 @@ function updateTracker() {
             var item = line.replaceAll(/\r?\n? /g, "")
             if (locationLogic[item]) {
                checkData += `${locationLogic[item]}:::check\n`
+               checkTable[locationLogic[item]] = true
             }
          }
       }
@@ -179,6 +188,56 @@ function updateTracker() {
 
    transitionData += lastTransition
    fs.writeFile(output, `\`\`\`mermaid\nflowchart TD\n${classDefs}\n\n${transitionData}\n${checkData}`, (err) => {
+      if (err) throw err
+   })
+}
+
+function updateLocation() {
+   const r_transitionChange = /(?<=\[INFO\]:\[Hkmp\.Game\.Client\.ClientManager\] ).*(?=\n|$)/gm
+   const modLogFile = fs.readFileSync(modLog, 'utf-8')
+   const location = modLogFile.match(r_transitionChange)?.at(-1).match(/\b(\w+)$/)[0]
+   if (!location) { return }
+
+   var doors = transitionTable[location]
+   var secondLayer = []
+   var transitionData = ``
+   for (const [fromDoor, toId] of Object.entries(doors)) {
+         var nameFrom = special[location]?.[0] ?? location
+         var nameTo = special[toId[0]]?.[0] ?? toId[0]
+         transitionData += `${location}([${nameFrom}]) -- ${fromDoor} --> ${toId[0]}([${nameTo}])\n`
+         secondLayer.push(toId[0])
+         if (special[location]?.[1]) {
+            transitionData += `${location}:::${special[location][1]}\n`
+         }
+         if (special[toId[0]]?.[1]) {
+            transitionData += `${toId[0]}:::${special[toId[0]][1]}\n`
+         }
+   }
+   for (const location2 of secondLayer) {
+      doors = transitionTable[location2]
+      for (const [fromDoor, toId] of Object.entries(doors)) {
+         var nameFrom = special[location2]?.[0] ?? location2
+         var nameTo = special[toId[0]]?.[0] ?? toId[0]
+         transitionData += `${location2}([${nameFrom}]) -- ${fromDoor} --> ${toId[0]}([${nameTo}])\n`
+         if (special[location2]?.[1]) {
+            transitionData += `${location2}:::${special[location2][1]}\n`
+         }
+         if (special[toId[0]]?.[1]) {
+            transitionData += `${toId[0]}:::${special[toId[0]][1]}\n`
+         }
+      }
+   }
+   secondLayer.push(location)
+   for (const room of secondLayer) {
+      if (checkTable[room]) {
+         transitionData += `${room}:::check\n`
+      }
+      if (avaliableTransitionTable[room]) {
+         transitionData += `${room}:::transition\n`
+      }
+   }
+
+   fs.writeFile(lastOut, `\`\`\`mermaid\nflowchart TD\n${classDefs}\n\n${transitionData}`, (err) => {
       if (err) throw err
    })
 }
